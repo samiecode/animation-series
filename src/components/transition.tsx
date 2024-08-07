1d3edb90a66359d06aa84c130bc01743c384f70b
samiecode <samiekeyz02@gmail.com>
2024-08-07 14:40:53 +0100

    initial commit

---
+++ src/components/transition.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import {motion} from "framer-motion";
+
+export default function Transition() {
+  return (
+		<motion.div
+			className="box"
+			initial={{opacity: 0, scale: 0.5}}
+			animate={{opacity: 1, scale: 1}}
+			transition={{duration: 3}}
+		/>
+  );
+}
---
+++ src/components/variants.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Variants() {
+	return <div></div>;
+}
