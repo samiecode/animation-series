9f8f6791c6038728d0508ed3e8a4327936d3cc80
samiecode <samiekeyz02@gmail.com>
2024-08-08 06:33:50 +0100

    feat: added variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import {motion} from "framer-motion";
 import Transition from "@/components/transition";
 import KeyFrame from "@/components/keyframe";
+import { Variant } from "@/components/variants";
 
 export default function Home() {
   return (
-		<main className="min-h-screen w-full flex justify-center items-center">
-      {/* <Transition/> */}
-      <KeyFrame/>
+		<main className="">
+      <Transition/>
+      {/* <KeyFrame/> */}
+      {/* <Variant/> */}
 		</main>
   );
 }
--- src/components/keyframe/index.tsx
+++ src/components/keyframe/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
+import "./styles.css"
 
 export default function KeyFrame() {
 	return (
